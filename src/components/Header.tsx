82f796be15a17b49e7c6ecc5d7f3a8244f98632a
Uyonoh <turakiuyonoh@gmail.com>
2025-09-14 07:55:02 +0100

    modify profile and search ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,20 +2,23 @@ import { Search } from "lucide-react";
 
 function Header() {
     return (
-        <header className="grid grid-cols-5 p-4 font-bold text-xl border-b">
+        <header className="grid grid-cols-5 p-4 font-bold text-xl border-b gap-4">
             <div className="logo">MediTrack</div>
             <div className="current-page">Overview</div>
             <div className="blank"></div>
-            <div className="search flex items-center font-normal text-sm gap-2">
+            <div className="search flex items-center font-normal text-sm gap-2 justify-self-end">
                 <Search size={16} />
-                <input type="search" name="search" id="search" placeholder="Search..." />
+                <input className="w-full py-1 rounded-lg focus:outline-none focus:ring-1 focus:ring-green-500 transition"
+                type="search" name="search" id="search" placeholder="Search..." />
             </div>
-            <div className="profile flex gap-1 text-sm font-normal">
+            <div className="profile flex gap-1 text-sm font-normal justify-self-end">
                 <div className="photo">
-                    U
+                    <img className="w-10 h-10 rounded-full object-cover ring-2 ring-transparent transition duration-300 hover:ring-green-500"
+                    alt="profile photo"
+                    src="/src/assets/images/profile.jpg"/>
                 </div>
                 <div className="info flex flex-col">
-                    <span>Dr. Evelyn Reed</span>
+                    <span className="font-bold">Dr. Evelyn Reed</span>
                     <span>Pharmacist</span>
                 </div>
             </div>
